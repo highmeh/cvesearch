48df5edfcbda35bfce9bf83e7c8ffabae1e454eb
Jayme <jaymehancock@protonmail.com>
2017-06-28 17:56:12 -0700

    Updated keyword search

--- cvesearch.py
+++ cvesearch.py
@@ -13,7 +13,7 @@ local_cve_db = "cve_db.xml"
 
 def downloaddb():
 	if os.path.isfile(local_cve_db):
-		print("[+] CVE Database exists.")
+		print("[-] CVE Database exists.")
 		exit()
 	else:
 		print("[+] Downloading cve_db.xml. This may take a few minutes.")
@@ -36,20 +36,44 @@ def cvenumber_search(cvenumber):
 		reference = item.refs
 
 		if cvenumber in name.lower():
-			print("\n[+] Match found:\n----------------")
+			print("\033[1m" + "\n[+] Match found:\n----------------")
 			print("\033[1m" + "[+] CVE ID: " + "\033[0m" + "{0}".format(name))
 			print("\033[1m" + "[+] CVE Description: " + "\033[0m" + "{0}".format(desc))
-			for url in reference.ref:
-				print("\033[1m" + "[+] Reference: " + "\033[0m" + "{0}".format(url.cdata))
+			try:
+				for url in reference.ref:
+					print("\033[1m" + "[+] Reference: " + "\033[0m" + "{0}".format(url.cdata))
+			except:
+				print("[-] No references available for {0}".format(name))
+
+
+def keyword_search(searchtext):
+	obj = untangle.parse(local_cve_db)
+	print("[+] Searching for {0}, this may take a minute...".format(searchtext))
+
+	o = untangle.parse(local_cve_db)
+	for item in o.cve.item:
+		name = item['name']
+		desc = item.desc.cdata
+		reference = item.refs
+
+		if searchtext in desc.lower():
+			print("\033[1m" + "\n[+] Match found:\n----------------")
+			print("\033[1m" + "[+] CVE ID: " + "\033[0m" + "{0}".format(name))
+			print("\033[1m" + "[+] CVE Description: " + "\033[0m" + "{0}".format(desc))
+			try:
+				for url in reference.ref:
+					print("\033[1m" + "[+] Reference: " + "\033[0m" + "{0}".format(url.cdata))
+			except:
+				print("\033[1m" + "[-] No references available for " + "\033[0m" + "{0}".format(name))
 
 
 progdesc = "cvesearch allows for offline searching of CVEs."
 parser = argparse.ArgumentParser(description=progdesc)
 parser.add_argument('-d', action='store_true', default='False', 
                     help='Download the CVE Database.')
-parser.add_argument('-c', metavar='CVE Number', action='store', default='None', 
+parser.add_argument('-c', metavar='CVE Number', action='store', 
                     help='Search by CVE Number')
-parser.add_argument('-s', metavar='Search Term', action='store', default='none', 
+parser.add_argument('-s', metavar='Search Term', action='store', 
 					help='Search by CVE keyword')
 args = parser.parse_args()
 
@@ -59,7 +83,17 @@ if args.d is True:
 if args.c:
 	cvenumber = args.c
 	if os.path.isfile(local_cve_db):
+		print("[+] Parsing CVE data...")
 		cvenumber_search(cvenumber.lower())
+	if not os.path.isfile(local_cve_db):
+		print("[-] CVE Database is missing. Downloading...")
+		downloaddb()
+
+elif args.s:
+	searchtext = args.s.lower()
+	if os.path.isfile(local_cve_db):
+		print("[+] Parsing CVE data...")
+		keyword_search(searchtext)
 	if not os.path.isfile(local_cve_db):
 		print("[-] CVE Database is missing. Downloading...")
 		downloaddb()
